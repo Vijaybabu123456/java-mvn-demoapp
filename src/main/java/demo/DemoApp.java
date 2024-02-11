2a54d1f2a1531fcef6f87fce7ad8cfe4f8040e98
Vijaybabu123456 <vk433960@gmail.com>
2024-02-11 21:58:41 +0530

    updated mvn file

--- src/main/java/demo/DemoApp.java
+++ src/main/java/demo/DemoApp.java
@@ -2,7 +2,7 @@ package demo;
 
 public class DemoApp {
     public static void main(String[] args) {
-        System.out.println("Hello world! java coding is fun!!");
+        System.out.println("Hello world! java coding is fun!! \n maven is much needed tool" \working on javamvn project);
 
         Timestamp ts = new Timestamp();
         String currentTimestamp = ts.getTimestamp();
